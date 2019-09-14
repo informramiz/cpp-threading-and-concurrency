79acd6dbc7e13524652ba7f1cf19142d1c31a62f
Ramiz Raja <informramiz@gmail.com>
2019-09-14 17:51:47 +0500

    feat: Print number of cores

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,7 @@
 
 int main() {
     std::cout << "Hello concurrent world from main: Thread id = " << std::this_thread::get_id() << std::endl;
+    unsigned int coresInThisMachine = std::thread::hardware_concurrency();
+    std::cout << "This machine supports concurrency with " << coresInThisMachine << " cores." << std::endl;
     return 0;
 }
