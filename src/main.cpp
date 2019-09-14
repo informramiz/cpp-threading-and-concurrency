5c87c5948b91a2d99dc0cfc616195fee0c3fe0fe
Ramiz Raja <informramiz@gmail.com>
2019-09-14 18:27:19 +0500

    feat: Pass callable object to thread

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,27 @@ void threadFunction() {
     std::cout << "Finished work in thread" << std::endl;
 }
 
+class Vehicle {
+public:
+    void operator()() {
+        std::cout << "Vehicle object has been created" << std::endl;
+    }
+};
+
 int main() {
     std::cout << "Hello concurrent world from main: Thread id = " << std::this_thread::get_id() << std::endl;
     unsigned int coresInThisMachine = std::thread::hardware_concurrency();
     std::cout << "This machine supports concurrency with " << coresInThisMachine << " cores." << std::endl;
 
     //create thread
-    std::thread thread(threadFunction);
+    Vehicle vehicle;
+    std::thread thread(vehicle);
     
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
     std::cout << "Finished worked in main" << std::endl;
 
     //wait for thread to finish
-    thread.detach();
-    // thread.join();
+    // thread.detach();
+    thread.join();
     return 0;
 }
